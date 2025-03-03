5dbb226c7afa1aa3621951f14be9479618dce0b2
khagani.abdullayev <khagani.abdullayev@naa.edu.az>
2025-03-03 16:53:16 +0400

    first problems

---
+++ Lesson1/GuessTheNumber.py
@@ -0,0 +1,20 @@
+import random
+
+n=random.randrange(0,20,1)
+found= False
+print(n)
+for i in range(1,4):
+    a=int(input())
+    if a==n:
+        print("Correct")
+        found=True
+        break
+    elif a<n:
+        print("Too low")
+
+    else:
+        print("Too high")
+if found:
+    print("Winner")
+else:
+    print("Looser")
